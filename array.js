d475ee3ce7690678ce5ac187b6421f8532be675e
Ali Hamza <alihanza85@gmail.com>
2026-04-19 21:26:38 +0500

    Node js Day 1 js basic Revision

---
+++ array.js
@@ -0,0 +1,12 @@
+//array js
+//array is a data structure that can hold multiple values at once. It is a collection of elements, each identified by an index or key. Arrays can be of fixed size or dynamic size, depending on the programming language being used. They are commonly used to store and manipulate lists of data, such as numbers, strings, or objects.
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr); // Output: [1, 2, 3, 4, 5]
+console.log(arr[0]) // Output: 1
+
+let fruits = ["Apple", "Banana", "Cherry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
