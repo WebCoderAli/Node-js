abadcee73d1e258418e4839924bdcd3fdd7b2bcc
Ali Hamza <devwithali@gmail.com>
2026-06-16 15:31:35 +0500

    Add files via upload

---
+++ Hello World/script.js
@@ -0,0 +1,9 @@
+console.log("Hello, World! This is my first node js script.");
+
+// Function to add two numbers
+function sum (num1 , num2){
+    return num1 + num2;
+}
+
+let result = sum(10 , 20); // 30
+console.log(result);
