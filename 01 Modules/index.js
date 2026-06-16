4638b3d8cfe6e7bf9e4fd26bc03a33b6af355e58
Ali Hamza <devwithali@gmail.com>
2026-06-16 16:45:13 +0500

    Add files via upload

--- 01 Modules/index.js
+++ 01 Modules/index.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require('fs'); // File System module Built-in module in Node.js
 
 const content = fs.readFileSync('./notes.txt', 'utf-8');
 console.log(content);
+
