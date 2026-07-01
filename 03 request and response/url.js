b0df1c04904c916ec942b0ab1e63f0155e71eb7c
Ali Hamza <devwithali@gmail.com>
2026-07-01 14:31:11 +0500

    Add files via upload

---
+++ 03 request and response/url.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Home {Page</title></head>');
+    res.write('<body><h1>This is home page</h1></body>');
+    res.write('</html>');
+    return res.end();
+  } else if (req.url === '/products') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Products Page</title></head>');
+    res.write('<body><h1>This is the products page</h1></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Not Found</title></head>');
+  res.write('<body><h1>Page not found</h1></body>');
+  res.write('</html>');
+  res.end();
+});
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
