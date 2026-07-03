e935cef33984c841e9950137a030146799a92749
Ali Hamza <devwithali@gmail.com>
2026-07-03 17:43:14 +0500

    Add files via upload

---
+++ 05 Event loop/user/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const requestHandler = require('./user');
+
+const server = http.createServer(requestHandler);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
