6abbf3adbea985e46aa5c35795b54d54cf87fdf4
Ali Hamza <devwithali@gmail.com>
2026-07-06 14:47:01 +0500

    Add files via upload

---
+++ 08 Express js/pratice/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req , res, next) => {
+  console.log('In the first middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In the second middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In the third middleware!');
+  next();
+});
+
+app.get("/", (req, res , next) => {
+  res.send(`<h1>Home Page</h1> <br>
+    <a href="/contact-us">Contact Us</a>`);
+});
+
+app.get("/contact-us", (req, res , next) => {
+  res.send(`<h1>Contact Us Page</h1> 
+    <br>
+    <form action="/contact-us" method="POST">
+        <input type="text" name="name" placeholder="Your Name">
+        <input type="email" name="email" placeholder="Your Email">
+        <button type="submit">Submit</button>
+    </form>
+    <br>
+    <a href="/">Home</a>`);
+});
+
+app.post("/contact-us", (req, res , next) => {
+  res.send(`<h1>Thank you for contacting us!</h1> 
+    <br>
+    <a href="/">Home</a>`);
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
