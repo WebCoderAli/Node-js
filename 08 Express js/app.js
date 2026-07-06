1c7b652e1d3a662edb8272b9b8819e8f0befa932
Ali Hamza <devwithali@gmail.com>
2026-07-06 13:59:28 +0500

    Add files via upload

--- 08 Express js/app.js
+++ 08 Express js/app.js
@@ -1,10 +1,27 @@
-const http = require('http');
 
 const userHandler = require('./user');
 
-const server = http.createServer(userHandler);
+const express = require('express');
+
+const app = express();
+
+app.use("/", (req , res, next) => {
+  console.log('In the first middleware!');
+  next(); // This will pass the request to the next middleware in line
+});
+
+app.post("/submit-user", (req, res, next) => {
+  console.log('In the second middleware!');
+  res.send('<h1>Submitted!</h1>');
+});
+
+app.get("/", (req, res , next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
 
 const PORT = 3000;
-server.listen(PORT, () => {
+
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
